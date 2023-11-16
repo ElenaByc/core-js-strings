a80aea5871e8f2146fe09a3427a71b7ad9fd7bb3
Elena Bychenkova <elena.bychenkova@gmail.com>
2023-11-15 19:21:07 -0800

    feat: countVowels

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -301,8 +301,30 @@ function containsSubstring(str, substring) {
  *   countVowels('aEiOu') => 5
  *   countVowels('XYZ') => 1
  */
-function countVowels(/* str */) {
-  throw new Error('Not implemented');
+function countVowels(str) {
+  const vowels = new Set([
+    'a',
+    'e',
+    'i',
+    'o',
+    'u',
+    'y',
+    'A',
+    'E',
+    'I',
+    'O',
+    'U',
+    'Y',
+  ]);
+  let counter = 0;
+
+  for (let i = 0; i < str.length; i += 1) {
+    if (vowels.has(str.charAt(i))) {
+      counter += 1;
+    }
+  }
+
+  return counter;
 }
 
 /**
